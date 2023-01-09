8832cea5b19aee92924b4928467795d71ea4c7fd
angeluray <aeucampos@gmail.com>
2023-01-09 17:08:49 -0400

    add city controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :cities
+    end
+  end
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
