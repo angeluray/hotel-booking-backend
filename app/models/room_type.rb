09f82ccdc5d0bcac4057e4242a8451687bbdca01
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-10 01:07:11 +0300

    Add hotels relation between RoomType

--- app/models/room_type.rb
+++ app/models/room_type.rb
@@ -1,4 +1,5 @@
 class RoomType < ApplicationRecord
-    has_many: :reservations
-    has_many: :users, through: :reservations
+  has_many: :reservations
+  has_many: :users, through: :reservations
+  has_many: :hotels, through: :reservations
 end
