f5fabad4b2cf08b2e0d2679a883732f1e1a9d78e
angeluray <aeucampos@gmail.com>
2023-01-09 22:37:13 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_09_201328) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_10_013203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hotels", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "rating"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "city_id", null: false
+    t.index ["city_id"], name: "index_hotels_on_city_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "hotel_id", null: false
+    t.bigint "room_type_id", null: false
+    t.index ["hotel_id"], name: "index_reservations_on_hotel_id"
+    t.index ["room_type_id"], name: "index_reservations_on_room_type_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "room_types", force: :cascade do |t|
+    t.string "type"
+    t.text "description"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +61,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_201328) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "hotels", "cities"
+  add_foreign_key "reservations", "hotels"
+  add_foreign_key "reservations", "room_types"
+  add_foreign_key "reservations", "users"
 end
