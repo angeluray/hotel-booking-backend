c1bb40b295823471d943ef56e44eece39501a0e4
angeluray <aeucampos@gmail.com>
2023-01-10 13:59:40 -0400

    Add application controller methods for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate
+
+    def authenticate
+        if request.headers["Authorization"]
+            begin
+                auth_header = request.headers["Authorization"]
+                decoded_token = JWT.decode(token, secret)
+                payload = decoded_token.first
+                user_id = payload["user_id"]
+                @user = User.find(user_id)
+            rescue => exception
+                render json: { message: "Error: #{exception}" }, status: :forbidden
+            end
+        else
+            render json: { message: "No authorization header sent"}, status: :forbidden
+        end
+    end
+
+    def token
+        auth_header.split(" ")[1]
+    end
+
+    def secret
+        secret = ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base 
+    end
+
+    def create_token
+        JWT.encode(payload, secret)
+    end
 end
