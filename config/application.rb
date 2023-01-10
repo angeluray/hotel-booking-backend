77b6d70eb753ae98a488992dd54c43253fb117e1
angeluray <aeucampos@gmail.com>
2023-01-10 17:41:04 -0400

    Fix autoload paths

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module HotelBookingBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.autoload_paths << "#{Rails.root}/lib"
 
     # Configuration for the application, engines, and railties goes here.
     #
