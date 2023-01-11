23a958c00bc187daa171fca3ef2a4702922433b5
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-11 16:38:25 +0300

    Fix rubocop errors

--- app/serializers/hotel_serializer.rb
+++ app/serializers/hotel_serializer.rb
@@ -2,10 +2,10 @@ class HotelSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
   attributes :id, :name, :description, :rating, :image
   def image
-    if object.image.attached?
-      {
-        url: rails_blob_url(object.image, only_path: true)
-      }
-    end
+    return unless object.image.attached?
+
+    {
+      url: rails_blob_url(object.image, only_path: true)
+    }
   end
 end
