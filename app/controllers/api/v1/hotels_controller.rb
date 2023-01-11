c9f70ca2c3cfed115cf83d3e02ff30745219d71f
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-11 14:38:13 +0300

    Add and update all action for  hotel controller

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -4,54 +4,55 @@ class Api::V1::HotelsController < ApplicationController
 
   # GET /api/v1/hotels
   def index
-    @api_v1_hotels = Api::V1::Hotel.all
-
-    render json: @api_v1_hotels
+    @hotels = Hotel.all
+    render json: @hotels
   end
 
   # GET /api/v1/hotels/1
   def show
-    render json: @api_v1_hotel
+    @hotel = Hotel.find(params[:id])
+      render json: @hotel
   end
 
   # POST /api/v1/hotels
   def create
-    @api_v1_hotel = Api::V1::Hotel.new(api_v1_hotel_params)
+    @hotel = Hotel.new(hotel_params)
 
-    if @api_v1_hotel.save
-      render json: @api_v1_hotel,
-             status: :created, location: @api_v1_hotel
-    else
-      render json: @api_v1_hotel.errors,
-             status: :unprocessable_entity
+      if @hotel.save
+        render json: @hotel, status: :created
+      else
+        render json: @hotel.errors, status: :unprocessable_entity
+      end
     end
-  end
 
   # PATCH/PUT /api/v1/hotels/1
   def update
-    if @api_v1_hotel.update(api_v1_hotel_params)
-      render json: @api_v1_hotel
-    else
-      render json: @api_v1_hotel.errors,
-             status: :unprocessable_entity
+    @hotel = Hotel.find(params[:id])
+
+      if @hotel.update(hotel_params)
+        render json: @hotel
+      else
+        render json: @hotel.errors, status: :unprocessable_entity
+      end
     end
-  end
 
   # DELETE /api/v1/hotels/1
   def destroy
-    @api_v1_hotel.destroy
+    @hotel = Hotel.find(params[:id])
+      @hotel.destroy
+      render json: { message: 'Hotel has successfully been deleted' }
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_api_v1_hotel
-    @api_v1_hotel = Api::V1::Hotel.find(params[:id])
+  def set_hotel
+    @hotel = Hotel.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
-  def api_v1_hotel_params
-    params.require(:api_v1_hotel).permit(:name,
+  def hotel_params
+    params.permit(:name,
                                          :description, :rating, :image)
   end
 end
