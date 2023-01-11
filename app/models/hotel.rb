a09ec4591ef40fd85b0dd91239e8fbe98fee8c69
Tadesse Jemal <tadessejemal63@gmail.com>
2023-01-11 14:14:07 +0300

    add validation for hotel

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,4 +2,10 @@ class Hotel < ApplicationRecord
   belongs_to :city
   has_many :reservations
   has_many :users, through: :reservations
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
+  validates :image, presence: true
+  validates :city_id, presence: true
 end
