ea1167e6688ae2750264a5ff0fad5636f12cd8a6
Tadesse Jemal <tadessejemal63@gmail.com>
2023-01-11 14:13:26 +0300

    add validation for room type

--- app/models/room_type.rb
+++ app/models/room_type.rb
@@ -2,4 +2,8 @@ class RoomType < ApplicationRecord
   has_many :reservations
   has_many :hotels, through: :reservations
   has_many :users, through: :reservations
+
+  validates :type, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
