0fdb5ebbb2233d556c1bf2660b8bef1b0011fe50
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-11 15:07:29 +0300

    Update room_type controller actions

--- app/controllers/api/v1/room_types_controller.rb
+++ app/controllers/api/v1/room_types_controller.rb
@@ -17,23 +17,23 @@ class Api::V1::RoomTypesController < ApplicationController
 
   # POST /api/v1/room_types
   def create
-    @api_v1_room_type = Api::V1::RoomType.new(api_v1_room_type_params)
+    @room_type = RoomType.new(room_type_params)
 
-    if @api_v1_room_type.save
-      render json: @api_v1_room_type,
-             status: :created, location: @api_v1_room_type
+    if @room_type.save
+      render json: @room_type,
+             status: :created, location: @room_type
     else
-      render json: @api_v1_room_type.errors,
+      render json: @room_type.errors,
              status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /api/v1/room_types/1
   def update
-    if @api_v1_room_type.update(api_v1_room_type_params)
-      render json: @api_v1_room_type
+    if @room_type.update(room_type_params)
+      render json: @room_type
     else
-      render json: @api_v1_room_type.errors,
+      render json: @room_type.errors,
              status: :unprocessable_entity
     end
   end
@@ -46,13 +46,13 @@ class Api::V1::RoomTypesController < ApplicationController
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_api_v1_room_type
-    @api_v1_room_type = Api::V1::RoomType.find(params[:id])
+  def set_room_type
+    @room_type = RoomType.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
-  def api_v1_room_type_params
-    params.require(:api_v1_room_type).permit(
+  def room_type_params
+    params.require(:room_type).permit(
       :type, :description, :price
     )
   end
