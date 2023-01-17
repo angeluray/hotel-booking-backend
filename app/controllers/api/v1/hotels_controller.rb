5ef1eaa1d92f14bddd512880cefc6ca045c9e7d2
Tadesse Jemal <tadessejemal63@gmail.com>
2023-01-17 16:09:28 +0300

    update hotel controller to fix upload images

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -4,21 +4,37 @@ class Api::V1::HotelsController < ApplicationController
 
   # GET /api/v1/hotels
   def index
-    @hotels = Hotel.all
+    # @hotels = Hotel.all.with_attached_images
+    @hotels = []
+    # render json: @hotels
+    Hotel.all.each do |hotel|
+      @hotels.push(hotel.as_json.merge({ image: url_for(hotel.image) }))
+    end
 
-    render json: @hotels
+    if @hotels.size.positive?
+      render json: @hotels
+    else
+      render json: { errors: 'Hotels not found' }, status: :not_found
+    end
   end
 
   # GET /api/v1/hotels/1
   def show
+    @result = []
     @hotel = Hotel.find(params[:id])
-    render json: @hotel
+    @result.push(@hotel.as_json.merge({ image: url_for(@hotel.image) }))
+
+    render json: @result
   end
 
   # POST /api/v1/hotels
   def create
     @hotel = Hotel.new(hotel_params)
     @city = City.find(params['city_id'])
+    # @hotel.images.attached(params[@hotel][:images])
+    # @hotel.images.attach(params[:images])
+    # @images = params[:images]
+
 
     @hotel.city = @city
 
