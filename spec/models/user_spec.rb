42ade6d308ea4fb93df43fbbe7632a8e9636e0e1
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-20 13:21:22 +0300

    Fix user model test errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,31 +1,25 @@
 require 'rails_helper'
 
-# RSpec.describe User, type: :model do
+RSpec.describe User, type: :model do
+    let(:user) { User.new(name: 'John Doe', username: 'johndoe', email: 'john@gmail.com', password: '123456') }
 
-#     user { User.new(name: 'Tadesse Jemal', username: 'Tadesse1' email: 'tadesse@gmail.com', password: 'password', password_digest: 'password') }
-#     before { user.save }
 
-#     it 'is valid with valid attributes' do
-#       expect(user).to be_valid
-#     end
+    it 'is valid with valid attributes' do
+      expect(user).to be_valid
+    end
 
-#     it 'is not valid without a name' do
-#       user.name = nil
-#       expect(user).to_not be_valid
-#     end
+    it 'is not valid without a username' do
+      user.username = nil
+      expect(user).to_not be_valid
+    end
 
-#     it 'is not valid without a username' do
-#       user.username = nil
-#       expect(user).to_not be_valid
-#     end
+    it 'is not valid without email' do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
 
-#     it 'is not valid without email' do
-#       user.email = nil
-#       expect(user).to_not be_valid
-#     end
-
-#     it 'is not valid without password' do
-#       user.password = nil
-#       expect(user).to_not be_valid
-#     end
-# end
+    it 'is not valid without password' do
+      user.password = nil
+      expect(user).to_not be_valid
+    end
+end
