5240739fdfa4d47bbbc7d6a9a386aa41af57e50b
Tadesse Jemal <tadessejemal63@gmail.com>
2023-01-20 11:38:00 +0300

    unit test for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+# RSpec.describe User, type: :model do
+
+#     user { User.new(name: 'Tadesse Jemal', username: 'Tadesse1' email: 'tadesse@gmail.com', password: 'password', password_digest: 'password') }
+#     before { user.save }
+
+#     it 'is valid with valid attributes' do
+#       expect(user).to be_valid
+#     end
+
+#     it 'is not valid without a name' do
+#       user.name = nil
+#       expect(user).to_not be_valid
+#     end
+
+#     it 'is not valid without a username' do
+#       user.username = nil
+#       expect(user).to_not be_valid
+#     end
+
+#     it 'is not valid without email' do
+#       user.email = nil
+#       expect(user).to_not be_valid
+#     end
+
+#     it 'is not valid without password' do
+#       user.password = nil
+#       expect(user).to_not be_valid
+#     end
+# end
