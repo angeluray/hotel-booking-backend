1ee65de06206df77bc06b81ac2e4075a61519177
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-20 13:10:00 +0300

    Fix city_spec test

--- spec/models/room_type_spec.rb
+++ spec/models/room_type_spec.rb
@@ -5,18 +5,17 @@ RSpec.describe RoomType, type: :model do
     @room = RoomType.new(name: 'Studio', description: 'This is a nice room', price: 500)
   end
 
-  it 'is valid if name of room is nil' do
-    @room.name = nil
-    expect(@room).to_not be_valid
+  it 'is valid with valid attributes' do
+    expect(@room).to be_valid
   end
 
-  it 'is valid if description of room is nil' do
-    @room.description = nil
-    expect(@room).to_not be_valid
+  it 'is valid with description attribute ' do
+    @room.description = 'This is a nice room'
+    expect(@room).to be_valid
   end
 
-  it 'is valid if description of room is nil' do
-    @room.price = nil
-    expect(@room).to_not be_valid
+  it 'is valid with price' do
+    @room.price = 500
+    expect(@room).to be_valid
   end
 end
