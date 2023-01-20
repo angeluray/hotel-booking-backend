ebcdb2704f207e575d0a5326fd6efb4c8f98b6f7
Tadesse Jemal <tadessejemal63@gmail.com>
2023-01-20 12:47:28 +0300

    unit test room type

--- spec/models/room_type_spec.rb
+++ spec/models/room_type_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe RoomType, type: :model do
   before(:each) do
-    @room = Hotel.new(name: 'Studio', description: 'This is a nice room', price: 500)
+    @room = RoomType.new(name: 'Studio', description: 'This is a nice room', price: 500)
   end
 
   it 'is valid if name of room is nil' do
-    @room.name = 'Queen'
+    @room.name = nil
     expect(@room).to_not be_valid
   end
 
   it 'is valid if description of room is nil' do
-    @room.description = 'I do not like it'
+    @room.description = nil
     expect(@room).to_not be_valid
   end
 
