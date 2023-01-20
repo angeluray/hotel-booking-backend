6a7c30344a3c1ed697a8b8fe4cc6d1bad6622ff7
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-20 13:23:10 +0300

    Fix rubocop errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    let(:user) { User.new(name: 'John Doe', username: 'johndoe', email: 'john@gmail.com', password: '123456') }
+  let(:user) { User.new(name: 'Tadesse', username: 'Tadesse', email: 'Tadesse@gmail.com', password: '123456') }
 
 
-    it 'is valid with valid attributes' do
-      expect(user).to be_valid
-    end
+  it 'is valid with valid attributes' do
+    expect(user).to be_valid
+  end
 
-    it 'is not valid without a username' do
-      user.username = nil
-      expect(user).to_not be_valid
-    end
+  it 'is not valid without a username' do
+    user.username = nil
+    expect(user).to_not be_valid
+  end
 
-    it 'is not valid without email' do
-      user.email = nil
-      expect(user).to_not be_valid
-    end
+  it 'is not valid without email' do
+    user.email = nil
+    expect(user).to_not be_valid
+  end
 
-    it 'is not valid without password' do
-      user.password = nil
-      expect(user).to_not be_valid
-    end
+  it 'is not valid without password' do
+    user.password = nil
+    expect(user).to_not be_valid
+  end
 end
